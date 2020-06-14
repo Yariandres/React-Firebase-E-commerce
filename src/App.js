fafcc89a2c0019713d7cbfd198a7c49a06eba299
Yari Andres <yari.andres@gmail.com>
2020-06-14 22:55:14 +0200

    adding home page and sass to proj

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import HomePage from './homePage';
+
 function App() {
   return (
     <div>
-      <h1>Hello world</h1>
+      <HomePage/>
     </div>
   );
 }
