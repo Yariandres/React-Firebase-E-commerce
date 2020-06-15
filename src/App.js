c6bb3e7cffbe331c483834b42a246d638ea07c85
Yari Andres <yari.andres@gmail.com>
2020-06-15 23:27:51 +0200

    adding router dom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homePage';
@@ -6,7 +7,9 @@ import HomePage from './pages/homepage/homePage';
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Router>
+        <Route path={"/"} component={HomePage}/>
+      </Router>
     </div>
   );
 }
