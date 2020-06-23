ecf30126342f086086bf6175c33d9d7fc558311f
Yari Andres <yari.andres@gmail.com>
2020-06-23 13:37:26 +0200

    adding card dropdown

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import { auth } from '../../firebase/firebase.utils'
+import { auth } from '../../firebase/firebase.utils';
+import CardIcon from '../card-icon/cardIcon';
+import CartDropdown from '../cart-dropdown/cartDropdown';
 
 import './header.scss';
 import { ReactComponent as Logo} from '../../assets/original.svg';
@@ -22,16 +24,20 @@ const Header = ({ currentUser }) => (
         CONTACT
       </Link>
 
-      {
-        currentUser ? (
-          <div className='option' onClick={ () => auth.signOut() }>SIGN OUT</div>
-        ) : (
-          <Link className='option' to='/signin'>
-            SIGN IN
-          </Link>
-        )     
-      }
+      {currentUser ? (
+        <div className='option' onClick={ () => auth.signOut() }>SIGN OUT</div>
+      ) : (
+        <Link className='option' to='/signin'>
+          SIGN IN
+        </Link>
+      )}
+
+      <CardIcon/>
+      
     </div>
+
+    <CartDropdown/>
+
   </div>
 );
 
